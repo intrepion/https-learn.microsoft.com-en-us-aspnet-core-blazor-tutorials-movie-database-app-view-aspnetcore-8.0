2262dcd09081d483a88947d641619e6b2630c452
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-10 08:14:13 -0600

    gawk -i inplace "{ gsub(/PluralNamePlaceholder/, \"Cast Members\") }; { print }" BlazorWebAppMovies.AcceptanceTests/Admin/CastMemberAdminPages.cs

--- BlazorWebAppMovies.AcceptanceTests/Admin/CastMemberAdminPages.cs
+++ BlazorWebAppMovies.AcceptanceTests/Admin/CastMemberAdminPages.cs
@@ -11,7 +11,7 @@ public partial class CastMemberAdminPages : PageTest
     public async Task MainNavigation()
     {
         await Expect(Page).ToHaveTitleAsync("Home");
-        await Page.GetByRole(AriaRole.Link, new() { Name = "PluralNamePlaceholder" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Cast Members" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Cast Member List");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Cast Member Creation");
