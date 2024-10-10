e937beaf7ae73d21a847c0292d0506b5e969c167
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-10 08:14:51 -0600

    gawk -i inplace "{ gsub(/\/\/ JustModelPropertyPlaceholder/, \"public string Name1 { get; set; } = string.Empty;\n\ \ \ \ // JustModelPropertyPlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Models/CastMemberAdminEditModel.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Models/CastMemberAdminEditModel.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Models/CastMemberAdminEditModel.cs
@@ -6,6 +6,7 @@ public class CastMemberAdminEditModel
 {
     public Guid Id { get; set; }
 
+    public string Name1 { get; set; } = string.Empty;
     // JustModelPropertyPlaceholder
     // public string Title { get; set; } = string.Empty;
     // public ToDoList? ToDoList { get; set; }
