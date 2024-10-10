2bb46db8575899cda22842784b30df98852977b0
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-10 08:16:31 -0600

    gawk -i inplace "{ gsub(/TableNamePlaceholder/, \"Movies\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Configuration/MovieEntityTypeConfiguration.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Configuration/MovieEntityTypeConfiguration.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Configuration/MovieEntityTypeConfiguration.cs
@@ -7,13 +7,13 @@ public class MovieEntityTypeConfiguration : IEntityTypeConfiguration<Movie>
 {
     public void Configure(EntityTypeBuilder<Movie> builder)
     {
-        builder.ToTable("TableNamePlaceholder", x => x.IsTemporal());
+        builder.ToTable("Movies", x => x.IsTemporal());
 
         // EntityConfigurationCodePlaceholder
         // builder.Property(x => x.PropertyNamePlaceholder);
 
         builder.HasOne(x => x.ApplicationUserUpdatedBy)
-            .WithMany(x => x.UpdatedTableNamePlaceholder)
+            .WithMany(x => x.UpdatedMovies)
             .OnDelete(DeleteBehavior.Restrict);
     }
 }
