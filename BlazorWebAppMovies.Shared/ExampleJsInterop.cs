5e0c919edfc866e5b7971667d956f15c2d49a637
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-10 01:57:40 -0600

    dotnet format

--- BlazorWebAppMovies.Shared/ExampleJsInterop.cs
+++ BlazorWebAppMovies.Shared/ExampleJsInterop.cs
@@ -1,4 +1,4 @@
-using Microsoft.JSInterop;
+﻿using Microsoft.JSInterop;
 
 namespace BlazorWebAppMovies.Shared;
 
@@ -15,7 +15,7 @@ public class ExampleJsInterop : IAsyncDisposable
 
     public ExampleJsInterop(IJSRuntime jsRuntime)
     {
-        moduleTask = new (() => jsRuntime.InvokeAsync<IJSObjectReference>(
+        moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
             "import", "./_content/BlazorWebAppMovies.Shared/exampleJsInterop.js").AsTask());
     }
 
