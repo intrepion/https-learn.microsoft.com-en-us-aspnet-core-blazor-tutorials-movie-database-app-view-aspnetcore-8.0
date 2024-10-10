413e45c7491d73c31e6372d5d9bb5a3b93a0f90e
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-10 08:17:27 -0600

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"BlazorWebAppMovies\") }; { print }" BlazorWebAppMovies.Client/*

--- BlazorWebAppMovies.Client/Program.cs
+++ BlazorWebAppMovies.Client/Program.cs
@@ -1,6 +1,6 @@
-﻿using ApplicationNamePlaceholder.BusinessLogic.Services;
-using ApplicationNamePlaceholder.BusinessLogic.Services.Client;
-using ApplicationNamePlaceholder.Client;
+﻿using BlazorWebAppMovies.BusinessLogic.Services;
+using BlazorWebAppMovies.BusinessLogic.Services.Client;
+using BlazorWebAppMovies.Client;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
