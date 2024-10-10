25ee7f0ac342f603e7e4908ab528f92e39316849
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-10 08:14:56 -0600

    gawk -i inplace "{ gsub(/\/\/ CreatePropertyCodePlaceholder/, \"await Page.GetByTestId(\\\"castMemberAdminEditName2\\\").FillAsync(\\\"a castMember\\\");\n\ \ \ \ \ \ \ \ // CreatePropertyCodePlaceholder\") }; { print }" BlazorWebAppMovies.AcceptanceTests/Admin/CastMemberAdminPages.cs

--- BlazorWebAppMovies.AcceptanceTests/Admin/CastMemberAdminPages.cs
+++ BlazorWebAppMovies.AcceptanceTests/Admin/CastMemberAdminPages.cs
@@ -17,6 +17,7 @@ public partial class CastMemberAdminPages : PageTest
         await Expect(Page).ToHaveTitleAsync("Cast Member Creation");
 
         await Page.GetByTestId("castMemberAdminEditName1").FillAsync("a castMember");
+        await Page.GetByTestId("castMemberAdminEditName2").FillAsync("a castMember");
         // CreatePropertyCodePlaceholder
         // await Page.GetByTestId("castMemberAdminEditName").FillAsync("a castMember");
 
