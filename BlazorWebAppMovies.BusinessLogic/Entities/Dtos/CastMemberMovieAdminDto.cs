ba18ccbde0a3316a34e847530206111d26adc925
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-10 08:15:48 -0600

    gawk -i inplace "{ gsub(/\/\/ DtoPropertyPlaceholder/, \"public CastMember? CastMember { get; set; }\n\ \ \ \ // DtoPropertyPlaceholder\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Dtos/CastMemberMovieAdminDto.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Dtos/CastMemberMovieAdminDto.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Dtos/CastMemberMovieAdminDto.cs
@@ -5,6 +5,7 @@ public class CastMemberMovieAdminDto
     public string ApplicationUserName { get; set; } = string.Empty;
     public Guid Id { get; set; }
 
+    public CastMember? CastMember { get; set; }
     // DtoPropertyPlaceholder
     // public string Title { get; set; } = string.Empty;
     // public ToDoList? ToDoList { get; set; }
