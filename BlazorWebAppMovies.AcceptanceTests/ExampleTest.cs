43ab58f3239dd83934542e259d66293dbaa89ce8
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-10 08:17:19 -0600

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"BlazorWebAppMovies\") }; { print }" BlazorWebAppMovies.AcceptanceTests/*

--- BlazorWebAppMovies.AcceptanceTests/ExampleTest.cs
+++ BlazorWebAppMovies.AcceptanceTests/ExampleTest.cs
@@ -2,7 +2,7 @@
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 
-namespace ApplicationNamePlaceholder.AcceptanceTests;
+namespace BlazorWebAppMovies.AcceptanceTests;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
