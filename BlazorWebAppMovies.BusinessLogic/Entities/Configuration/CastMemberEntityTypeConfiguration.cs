67f7d211344dbfae105326d06bff3cf055ebeeeb
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-10 08:14:17 -0600

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"CastMember\") }; { print }" BlazorWebAppMovies.BusinessLogic/Entities/Configuration/CastMemberEntityTypeConfiguration.cs

--- BlazorWebAppMovies.BusinessLogic/Entities/Configuration/CastMemberEntityTypeConfiguration.cs
+++ BlazorWebAppMovies.BusinessLogic/Entities/Configuration/CastMemberEntityTypeConfiguration.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
 
-public class EntityNamePlaceholderEntityTypeConfiguration : IEntityTypeConfiguration<EntityNamePlaceholder>
+public class CastMemberEntityTypeConfiguration : IEntityTypeConfiguration<CastMember>
 {
-    public void Configure(EntityTypeBuilder<EntityNamePlaceholder> builder)
+    public void Configure(EntityTypeBuilder<CastMember> builder)
     {
         builder.ToTable("TableNamePlaceholder", x => x.IsTemporal());
 
