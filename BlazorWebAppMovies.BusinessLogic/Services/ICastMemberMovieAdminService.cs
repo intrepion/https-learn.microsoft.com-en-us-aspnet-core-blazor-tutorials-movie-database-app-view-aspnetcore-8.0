27ea3d5149568bde782db6e72f6200ee1164aaf8
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-10 08:15:31 -0600

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"CastMemberMovie\") }; { print }" BlazorWebAppMovies.BusinessLogic/Services/ICastMemberMovieAdminService.cs

--- BlazorWebAppMovies.BusinessLogic/Services/ICastMemberMovieAdminService.cs
+++ BlazorWebAppMovies.BusinessLogic/Services/ICastMemberMovieAdminService.cs
@@ -3,11 +3,11 @@ using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Services;
 
-public interface IEntityNamePlaceholderAdminService
+public interface ICastMemberMovieAdminService
 {
-    Task<EntityNamePlaceholderAdminDto?> AddAsync(EntityNamePlaceholderAdminDto castMemberMovie);
+    Task<CastMemberMovieAdminDto?> AddAsync(CastMemberMovieAdminDto castMemberMovie);
     Task<bool> DeleteAsync(string userName, Guid id);
-    Task<EntityNamePlaceholderAdminDto?> EditAsync(EntityNamePlaceholderAdminDto castMemberMovie);
-    Task<List<EntityNamePlaceholder>?> GetAllAsync(string userName);
-    Task<EntityNamePlaceholderAdminDto?> GetByIdAsync(string userName, Guid id);
+    Task<CastMemberMovieAdminDto?> EditAsync(CastMemberMovieAdminDto castMemberMovie);
+    Task<List<CastMemberMovie>?> GetAllAsync(string userName);
+    Task<CastMemberMovieAdminDto?> GetByIdAsync(string userName, Guid id);
 }
